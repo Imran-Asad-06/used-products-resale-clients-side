1c1ec0efa5097c3fcc264a92c5c8781a5de68619
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-11-28 20:15:55 +0600

    login and registration

---
+++ src/component/Pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import useTitle from '../../../Hooks/useTitle';
+import Header from '../../Shared/Header/Header';
+import Advertise from '../Advertise/Advertise';
+import Banner from '../Banner/Banner';
+import Brands from '../Brands/Brands';
+import Categories from '../Categories/Categories';
+import Proven from '../Proven/Proven';
+
+const Home = () => {
+    useTitle('home');
+    return (
+        <div>
+         <Banner></Banner>
+         <Advertise></Advertise>
+         <Categories></Categories>
+         <Proven></Proven>
+        </div>
+    );
+};
+
+export default Home;
